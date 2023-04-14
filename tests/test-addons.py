1c128525b3eea179b67b258f3656165042788d59
Albert Skalt <allber971@gmail.com>
2023-04-14 12:26:29 +0300

    Feat: create appsmith addon

---
+++ tests/test-addons.py
@@ -0,0 +1,28 @@
+import sh
+import yaml
+
+from utils import (
+    microk8s_enable,
+    wait_for_pod_state,
+    microk8s_disable,
+    kubectl_get
+)
+
+
+def addon_info(status, name):
+    for addon in status['addons']:
+        if addon['name'] == name:
+            return addon
+    return None
+
+
+class TestAddons(object):
+    def test_appsmith(self):
+        NAME = 'appsmith-k8s'
+        microk8s_enable(NAME)
+        #wait_for_pod_state("appsmith-0", "default", "running")
+        print(kubectl_get("all"))
+        status = yaml.safe_load(sh.microk8s.status(format="yaml"))
+        info = addon_info(status, NAME)
+        assert info['status'] == 'enabled'
+        microk8s_disable(NAME)
