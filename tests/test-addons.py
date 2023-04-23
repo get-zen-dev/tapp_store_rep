a86d541753db8007e94f30417d13de514638484b
Korinin38 <artkash@bk.ru>
2023-04-23 13:56:22 +0300

    feat: add n8n addon

--- tests/test-addons.py
+++ tests/test-addons.py
@@ -20,7 +20,16 @@ class TestAddons(object):
     def test_appsmith(self):
         NAME = 'appsmith-k8s'
         microk8s_enable(NAME)
-        #wait_for_pod_state("appsmith-0", "default", "running")
+        # wait_for_pod_state("appsmith-0", "default", "running")
+        print(kubectl_get("all"))
+        status = yaml.safe_load(sh.microk8s.status(format="yaml"))
+        info = addon_info(status, NAME)
+        assert info['status'] == 'enabled'
+        microk8s_disable(NAME)
+
+    def test_n8n(self):
+        NAME = 'n8n-k8s'
+        microk8s_enable(NAME)
         print(kubectl_get("all"))
         status = yaml.safe_load(sh.microk8s.status(format="yaml"))
         info = addon_info(status, NAME)
